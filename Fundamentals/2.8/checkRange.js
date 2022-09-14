b8808694538368e29b53ccd8b9214bcdb6753529
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-09-14 11:21:02 -0300

    Manejo de condicionales

---
+++ Fundamentals/2.8/checkRange.js
@@ -0,0 +1,8 @@
+let edad = 10
+
+if (edad >= 14 && edad <= 90) {
+    console.log(`Usted esta en el rango entre 14 o 90, felicitaciones su edad es: ${edad}`) 
+} else {
+    console.log('Usted no esta en el rango entre 14 o 90, listo no nos interesa nada mas')
+}
+
---
+++ Fundamentals/2.8/checkRangeOutside.js
@@ -0,0 +1,13 @@
+let edad = 110
+
+if (!(edad >= 14 && edad <= 90)) {
+    console.log(`La edad no esta entre 14 y 90, bien ahi`) 
+} else {
+    console.log('Fooo')
+}
+
+if (edad < 14 || edad > 90) {
+    console.log(`La edad no esta entre 14 y 90, bien ahi`) 
+} else {
+    console.log('Fooo')
+}
