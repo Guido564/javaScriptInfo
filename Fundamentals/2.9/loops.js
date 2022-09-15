e8c16794229cb65f68ca27b5020ec73641d9ac0d
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-09-15 11:52:39 -0300

    Finalizado

--- Fundamentals/2.9/loops.js
+++ Fundamentals/2.9/loops.js
@@ -1,12 +1,26 @@
+// loguear even numbers
 for (let i = 0; i < 11; i++) {
     if (i % 2 == 0) {
         console.log(i)
     }
 }
 
+// loop pasado a while
 let i = 0
 
 while(i < 3){
     console.log(i)
     i++
+}
+
+// buscar numeros primos
+let n = 10;
+
+nextPrime:
+for (let i = 2; i <= n; i++) {
+    for (let j = 2; j < i; j++) {
+        if (i % j == 0) continue nextPrime;
+    }
+
+    console.log(i); 
 }
